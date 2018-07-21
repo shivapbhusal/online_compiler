664cf000355d9e70e7012aaf0d44c7ea782675bc
shivapbhusal <civabhusal@gmail.com>
2018-07-21 19:03:57 -0400

    add code to write the text into file

--- src/myApp/app.js
+++ src/myApp/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
-
+const fs = require('fs');
+var spawn = require('child_process').spawn;
 const app = express();
 
 // Middleware / routing
@@ -21,8 +22,19 @@ app.get('/form-with-post',function(request, response){
 });
 
 app.get('/submit-form-with-get', function(request, response){
-	return response.send(request.query.source_code); // ?first=Abc&Last=xyz
-})
+	// Write the code to a text file.
+
+	fs.appendFile('temp.py', request.query.source_code, (err) => {
+		if (err) throw err;
+	});
+	var compile = spawn('python', ['temp.py']);
+    var result;
+	compile.stdout.on('data', function (data) {
+    	result = data; 
+	});
+	// execute the code. 
+	return response.send(result); 
+});
 
 app.listen(3000, function(){
 	console.log('My server is running on port 3000.')
