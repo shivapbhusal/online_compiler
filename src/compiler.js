a7fcb451cb996b14f9ba74be7ee143ca8ff82686
shivapbhusal <civabhusal@gmail.com>
2018-07-20 23:58:27 -0400

    edit to get the result in browser

--- src/compiler.js
+++ src/compiler.js
@@ -1,12 +1,22 @@
 /*
  An online compiler
 */
-
+var http = require("http");
 var spawn = require('child_process').spawn;
 var compile = spawn('python', ['temp.py']);
+var result;
 
 compile.stdout.on('data', function (data) {
-    console.log('Result: ' + data);
+    result = data; 
 });
+
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end(result);
+}).listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
+
+
 
 
